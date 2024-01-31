ddda6bdd23d0719d159bcc8755eb71003946ec49
Hammadawan424 <Hammadawan9255@gmail.com>
2024-01-31 08:09:49 +0500

    Animations, Sqlalchemy, flexbox

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from flask import Flask, render_template, session, redirect, request
 from flask_session import Session
-from helpers import login as logn
+from helpers import login, users, mails
+import logging
+from sqlalchemy import insert, create_engine
+
 
 app = Flask(__name__)
 
@@ -10,18 +13,44 @@ app.config["SESSION_FILE_DIR"] = "./session"
 app.config["SESSION_PERMANENT"] = False
 Session(app)
 
+werkzeug_log = logging.getLogger('werkzeug')
+werkzeug_log.disabled = True
+
 @app.route("/")
-@logn
+@login
 def hello():
     return render_template("index.html")
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
-    if request.method == "GET":
-        return "YOU CAME HERE BY GET"
+    # manages for get request
+    if request.method == "GET" and session.get("id") == None:
+        return render_template("login.html")
+    elif request.method == "GET":
+        return render_template("index.html")
+    
+    #manages for post request
     else:
-        if session.get("id"):
-            return redirect("/index")
-        else:
-            session["id"] = request.form.get("username")
-            return redirect("/")
+        session["id"] = request.form.get("username")
+        return redirect("/")
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if request.method == "GET":
+        return render_template("register.html")
+    
+    #manages post request    
+    data = request.form.get("name"), request.form.get("pass"), request.form.get("con_pass")
+
+    if data[1] != data[2]:
+        return "Please provide same password"
+    if len(data[1]) < 8:
+        return "Atleast 8 characters are required"
+    
+    stmt = insert(users).values(username=data[0], password=data[1])
+    engine = create_engine("sqlite:///database.db", echo=True)
+    with engine.begin() as conn:
+        result = conn.execute(stmt)
+        session["id"] = result.lastrowid
+        return redirect("/")
--- helpers.py
+++ helpers.py
@@ -1,8 +1,69 @@
-from flask import session, render_template
+from flask import session, redirect
+from sqlalchemy import Table, Column, MetaData, Integer, String, Boolean, ForeignKey, create_engine, insert
+
 def login(f):
     def inner(*args,  **kwargs):
         if session.get("id") is None:
-            return render_template("login.html")
+            return redirect("/login")
         else:
             return f(*args,  **kwargs)
     return inner
+
+
+engine = create_engine("sqlite:///database.db", echo=True)
+metadata = MetaData()
+
+users = Table(
+    "users", 
+    metadata,
+    Column("id", Integer, primary_key=True),
+    Column("username", String(30)), #uniqute constraint
+    Column("password", String)
+)
+
+mails = Table(
+    "mails",
+    metadata,
+    Column("id", Integer, primary_key=True),
+    Column("text", String),
+    Column("sender", ForeignKey("users.id"), nullable=False),
+    Column("receiver", ForeignKey("users.id"), nullable=False),
+    Column("date", String), #dates in alchemy    
+    Column("read", Boolean, default=False)
+)
+
+def engine_chekcer(obj):
+    if obj.engine == None:
+        raise ValueError("Missing Engine")
+    else:
+        return obj.engine
+
+class Email:
+
+    engine = None
+
+    @classmethod
+    def set_engine(cls, engine):
+        cls.engine = engine
+
+    def __init__(self, message, sender, receiver):
+        self.message = message
+        self.sender = sender
+        self.receiver = receiver
+
+    def set(self):
+        engine = engine_chekcer(self)
+        with engine.begin() as conn:
+            stmt = insert(mails).values(text=self.text, sender=self.sender, receiver=self.receiver)
+            conn.execute(stmt)
+
+    def read(self):
+        ...
+
+    def get(self):
+        ...
+
+
+if __name__ == "__main__":
+    print("Metadata was called")
+    metadata.create_all(engine)
